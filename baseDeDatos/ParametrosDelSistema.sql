8c37b7eeb131badd12aa4b2dd602c2f58e75d845
rapardoc <rapardoc@outlook.com>
2025-08-26 10:43:25 -0600

    ajustes tabla Parametros

--- baseDeDatos/ParametrosDelSistema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ParametrosDelSistema (
-    ID INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador único del parámetro',
-    CODIGO VARCHAR(100) NOT NULL UNIQUE COMMENT 'Código del parámetro',
-    DESCRIPCION VARCHAR(255) COMMENT 'Descripción del parámetro a configurar',
-    VALOR TEXT COMMENT 'Valor asignado al parámetro',
-    FECHA_CREACION TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha de última modificación del parámetro',
-    VIGENTE BOOLEAN DEFAULT TRUE COMMENT 'TRUE = parámetro vigente, FALSE = histórico'
-);
---
+++ baseDeDatos/parametros.sql
@@ -0,0 +1,8 @@
+CREATE TABLE parametros (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador unico del parametro',
+    codigo VARCHAR(100) NOT NULL UNIQUE COMMENT 'Codigo del parametro',
+    descripcion VARCHAR(255) COMMENT 'Descripcion del parametro a configurar',
+    valor TEXT COMMENT 'Valor asignado al parametro',
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha de ultima modificacion del parametro',
+    vigente BOOLEAN DEFAULT TRUE COMMENT 'TRUE = parametro vigente, FALSE = historico'
+)COMMENT='Tabla que almacena los parametros configurables del sistema';
