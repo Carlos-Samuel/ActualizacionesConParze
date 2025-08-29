b9b5fc7077c907c58798631cee4534e212a27110
rapardoc <rapardoc@outlook.com>
2025-08-29 09:57:13 -0600

    Ajustes de Bases de Datos

--- baseDeDatos/Bitacora.sql
+++ baseDeDatos/Bitacora.sql
@@ -1,16 +1,16 @@
-CREATE TABLE bitacora_de_ejecuciones ()
+CREATE TABLE bitacora (
 		id_bitacora INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador unico de la bitacora',
-		tipo_de_cargue ENUM('FULL', 'DELTA') COMMENT 'tipo de cargue del envio realizado'
-		fecha_ejecucion DATE COMMENT 'Fecha en que se realiza el proceso'
-		hora_ejecucion TIME COMMENT 'Hora en que se realiza el proceso' 
-		origen_del_proceso ENUM('Manual', 'Automatico', 'Reenvio') COMMENT'Origen del proceso de envio'
-		cantidad_registros_enviados INTEGER COMMENT 'Cantidad de registros que se enviaron en el proceso'
-		tamaño_del_archivo VARCHAR COMMENT 'Tamaño del archivo enviado en el proceso' 
-        resultado_del_envio ENUM('Exitoso', 'Fallido') COMMENT 'resultado del envio exitoso o fallido'
-        descripcion_error VARCHAR COMMENT 'Descripcion del error en caso de fallo'
-        parametros_usados VARCHAR COMMENT 'Parametros usados en el proceso'
-		Fecha_hora_de_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha y hora en que se registro la ejecucion' 
-        satisfactorio BOOLEAN COMMENT 'TRUE = ejecucion satisfactoria, FALSE = ejecucion con errores'
-		ruta_archivo VARCHAR COMMENT 'Ruta donde se almacena el archivo generado'
+		tipo_de_cargue ENUM('FULL', 'DELTA') COMMENT 'tipo de cargue del envio realizado',
+		fecha_ejecucion DATE COMMENT 'Fecha en que se realiza el proceso',
+		hora_ejecucion TIME COMMENT 'Hora en que se realiza el proceso',
+		origen_del_proceso ENUM('Manual', 'Automatico', 'Reenvio') COMMENT'Origen del proceso de envio',
+		cantidad_registros_enviados INTEGER COMMENT 'Cantidad de registros que se enviaron en el proceso',
+		tamaño_del_archivo INTEGER COMMENT 'Tamaño del archivo enviado en el proceso', 
+        resultado_del_envio ENUM('Exitoso', 'Fallido') COMMENT 'resultado del envio exitoso o fallido',
+        descripcion_error VARCHAR(255) COMMENT 'Descripcion del error en caso de fallo',
+        parametros_usados VARCHAR(255) COMMENT 'Parametros usados en el proceso',
+		Fecha_hora_de_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha y hora en que se registro la ejecucion', 
+        satisfactorio BOOLEAN COMMENT 'TRUE = ejecucion satisfactoria, FALSE = ejecucion con errores',
+		ruta_archivo VARCHAR(255) COMMENT 'Ruta donde se almacena el archivo generado',
 		Archivo_borrado BOOLEAN comment 'TRUE = archivo borrado despues de enviarse, FALSE = archivo no borrado'
-)COMMENT='Tabla que almacena la bitacora de ejecuciones del sistema';
+)COMMENT 'Tabla que almacena la bitacora de ejecuciones del sistema';
--- baseDeDatos/reportes.sql
+++ baseDeDatos/reportes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reportes (
 		id_reporte INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador unico del reporte',
 		id_bitacora INTEGER COMMENT 'REFERENCIA ID Bitacora de ejecuciones',
-		id_interno VARCHAR COMMENT 'Identificador unico del producto en AGIL',
+		id_interno VARCHAR(255) COMMENT 'Identificador unico del producto en AGIL',
 		cantidad INTEGER COMMENT 'Cantidad de productos en el reporte',
 		precio_venta INTEGER COMMENT 'Precio de venta total del producto',
 		descuento INTEGER COMMENT 'Descuento aplicado al producto',
