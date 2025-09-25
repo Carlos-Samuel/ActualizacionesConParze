ab2681169977d3c4e9a117facb837227b185663f
rapardoc <rapardoc@outlook.com>
2025-09-25 13:02:02 -0600

    ajuste URL language

--- scripts/bitacoras.js
+++ scripts/bitacoras.js
@@ -75,7 +75,8 @@ function cargarTabla(datosFiltro) {
             }
             }
         ],
-        language: { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' }
+        //language: { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' }
+        language: { url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' } 
         });
 
 
